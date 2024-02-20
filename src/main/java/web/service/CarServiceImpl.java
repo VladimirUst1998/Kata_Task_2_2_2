0e4c6a2e7e389fbf1fec9fd3532ad85491595855
VladimirUst1998 <vovaust1998@mail.ru>
2024-02-20 10:00:10 +0500

    Исправление замечаний.20.02.24

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,9 +1,27 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface CarServiceImpl {
-    List<Car> listOfCars(int count);
+@Component
+public class CarServiceImpl implements CarService{
+
+    private final List<Car> cars;
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car("Toyota", "Corolla", 111));
+        cars.add(new Car("Toyota", "Camry", 777));
+        cars.add(new Car("Lexus", "ES350", 999));
+        cars.add(new Car("Kia", "Stinger", 333));
+        cars.add(new Car("Lexus", "IS350", 174));
+    }
+
+    @Override
+    public List<Car> listOfCars(int count) {
+        int limitCount = 5;
+        return cars.stream().limit(Math.min(count, limitCount)).toList();
+    }
 }
